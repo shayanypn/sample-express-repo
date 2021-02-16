841cab1f268bbe8db21f707f370a9291650f4143
shayanypn <shayanypn@gmail.com>
2021-02-16 21:24:49 +0330

    adding nerd

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const {nerdvision} = require('@nerdvision/agent');
 const app = express()
-const port = 3000
+const port = 3000;
+
+nerdvision.init('aa899dcebf837cf83f6c0e8bb5f65a3cc20cff73cd51f90c2a14beb7e19ecde66f2b92717f60b455177549c7045876bf1f6586d338c029029ddaf2bdfdca730a');
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
