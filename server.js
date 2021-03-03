44fd5e148092148c61544fa8359ae3cad72e4e72
shayanypn <shayanypn@gmail.com>
2021-03-03 09:13:51 +0330

    modify the server

--- server.js
+++ server.js
@@ -1,22 +1,24 @@
 const express = require('express')
-const {nerdvision} = require('@nerdvision/agent');
+const { nerdvision } = require('@nerdvision/agent');
 const app = express()
 const port = 3000;
 
 nerdvision.init('aa899dcebf837cf83f6c0e8bb5f65a3cc20cff73cd51f90c2a14beb7e19ecde66f2b92717f60b455177549c7045876bf1f6586d338c029029ddaf2bdfdca730a');
 
 
-app.get('/', (req, res) => {
-	
-	throw Error('asdasdas');
+app.get('/sum/:num1/:num2', (req, res) => {
+    const { num1, num2 } = req.params;
 
-	res.send('Hello World!')
-
-
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    res.send(`Sum is: ${num2 - num1}`);
 });
 
 
+app.get('/', (req, res) => {
+    throw Error('asdasdas');
+    res.send('Hello World!')
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+});
